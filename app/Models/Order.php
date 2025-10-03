cd1b9db22dc2ca9a67a13b8e3a4d2c6ab87978c5
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-10-03 18:15:35 +0200

    feat: add models with relationships and HasFactory

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['client_id', 'order_date', 'status', 'total'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
