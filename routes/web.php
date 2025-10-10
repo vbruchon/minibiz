13b81139d33e4700ca40b021dbed397d61e75623
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-10-10 17:37:53 +0200

    fix(routes): correct customer routes to avoid /create conflict

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ Route::get('/', function () {
 
 Route::prefix('dashboard/customers')->name('customers.')->group(function () {
     Route::get('/', [CustomerController::class, 'index'])->name('index');
-    Route::get('/{customer}', [CustomerController::class, 'show'])->name('show');
     Route::get('/create', [CustomerController::class, 'create'])->name('create');
     Route::post('/store', [CustomerController::class, 'store'])->name('store');
-    Route::get('/edit/{customer}', [CustomerController::class, 'edit'])->name('edit');
-    Route::put('/update/{customer}', [CustomerController::class, 'update'])->name('update');
-    Route::delete('/delete/{customer}', [CustomerController::class, 'destroy'])->name('delete');
+    Route::get('/{customer}', [CustomerController::class, 'show'])->name('show');
+    Route::get('/{customer}/edit', [CustomerController::class, 'edit'])->name('edit');
+    Route::put('/{customer}', [CustomerController::class, 'update'])->name('update');
+    Route::delete('/{customer}', [CustomerController::class, 'destroy'])->name('delete');
 });
